541be3822ff63258ac91d3343fe4dc88dce6756a
Sonam Kumari <56336173+sonamkumari16@users.noreply.github.com>
2022-07-25 18:14:32 +0530

    Add files via upload

---
+++ theme_Changer/script.js
@@ -0,0 +1,24 @@
+function setblack() {
+    document.getElementById("part1").style.backgroundColor = "black";
+    document.getElementById("text").style.borderColor = "white";
+    document.getElementById("text").style.backgroundColor = "black";
+    document.getElementById("text").style.color = "white";
+  }
+  function setblue() {
+      document.getElementById("part1").style.backgroundColor = "blue";
+      document.getElementById("text").style.borderColor = "white";
+      document.getElementById("text").style.backgroundColor = "blue";
+      document.getElementById("text").style.color = "white";
+  }
+  function setgreen() {
+      document.getElementById("part1").style.backgroundColor = "green";
+      document.getElementById("text").style.borderColor = "black";
+      document.getElementById("text").style.backgroundColor = "green";
+      document.getElementById("text").style.color = "black";
+  }
+  function setyellow() {
+      document.getElementById("part1").style.backgroundColor = "yellow";
+      document.getElementById("text").style.borderColor = "black";
+      document.getElementById("text").style.backgroundColor = "yellow";
+      document.getElementById("text").style.color = "black";
+  }
