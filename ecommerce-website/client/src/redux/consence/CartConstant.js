ac517c5ab80572f590b13c042d800d1684d23350
Sonam Kumari <56336173+sonamkumari16@users.noreply.github.com>
2022-10-11 20:42:57 +0530

    Delete ecommerce-website directory

--- ecommerce-website/client/src/redux/consence/CartConstant.js
+++
@@ -1,2 +0,0 @@
-export const ADD_TO_CART='addtoCart';
-export const REMOVE_FROM_CART='removeFromCart'
--- ecommerce-website/client/src/redux/consence/Productconstant.js
+++
@@ -1,5 +0,0 @@
-export const GET_PRODUCT_SUCESS='getProductsSuccess'
-export const GET_PRODUCT_FAIL ='getProductsFail'
-
-export const GET_PRODUCT_DETAIL_SUCESS='getProductDetailSuccess'
-export const GET_PRODUCT_DETAIL_FAIL ='getProductDetailFail'
